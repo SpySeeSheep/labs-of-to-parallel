d41c491cd307fac992417befc66c59c4c5327d44
Роман Гилязов <gilyazov.roman2015@yandex.ru>
2020-02-12 15:17:39 +0500

    Add lab1

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,113 @@
+﻿// lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <time.h>
+#include <stdio.h>
+#include <chrono>
+#include <algorithm>
+#include <iostream>
+
+double* create_array(size_t &count)
+{
+	try {
+		srand(time(NULL));
+		double* arr = new double[count];
+		
+		//#pragma omp parallel for
+		for (size_t i = 0; i < count; ++i)
+		{
+			arr[i] = (double)( rand() % 101 ) - 50.0 ; // генерация чисел от -50 до 50
+		}
+
+		return arr;
+	}
+	catch (...)
+	{
+		printf("error!");
+		return nullptr;
+	}
+}
+
+double** create_array(const size_t& str, const size_t& col)
+{
+	double** nw = new double* [str];
+	for (int i = 0; i < str; ++i)
+	{
+		nw[i] = new double[col];
+	}
+	return nw;
+}
+
+double sum_arr(const double* arr, const size_t& size)
+{
+	double sum = 0;
+	for (size_t i = 0; i < size; ++i)
+	{
+		if (arr[i] > 0.0)
+		{
+			sum += arr[i];
+		}
+	}
+	return sum;
+}
+
+double* get_array(const double* arr, const size_t& size)
+{
+	double* nw = new double[size];
+	for (int i = 0; i < size; ++i)
+	{
+		nw[i] = arr[i];
+	}
+	return nw;
+}
+
+void sum_plus_elelent_without_sort(const double* arr, const size_t& size)
+{
+	auto begin = std::chrono::high_resolution_clock::now();
+	double sum = sum_arr(arr, size);
+	auto end = std::chrono::high_resolution_clock::now();
+	long long time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+	printf("sum == %f, time == %I64d\n\n", sum, time);
+}
+
+void sum_plus_element_with_sort_notime(const double* arr, const size_t& size)
+{
+	double* nw = get_array(arr, size);
+	std::sort(nw, nw + size);
+	auto begin = std::chrono::high_resolution_clock::now();
+	double sum = sum_arr(arr, size);
+	auto end = std::chrono::high_resolution_clock::now();
+	long long time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+	printf("sum == %f, time == %I64d\n\n", sum, time);
+}
+
+void multy_matrix(const double** a, const size_t& str_a,const size_t& col_a, 
+	const double** b, const size_t& str_b, const size_t& col_b)
+{
+	double** res = create_array(str_a, col_b);
+}
+
+int main(int argc, char **argv)
+{
+	printf("Part 1\n");
+	size_t size = 10'000'000;
+	double* arr = create_array(size);
+
+	// Сумма положительных элементов без сортировки
+	sum_plus_elelent_without_sort(arr, size);
+	// Сумма положительных элементов с сортировкой (время измеряется с сортировкой)
+	sum_plus_element_with_sort_notime(arr, size);
+
+	printf("Part 2\n");
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
--- lab_zero/lab_zero/lab_zero.cpp
+++ lab_zero/lab_zero/lab_zero.cpp
@@ -1,38 +1,52 @@
 ﻿#include <stdio.h>
 #include <math.h>
 #include <omp.h>
-//#include <chrono>
+#include <chrono>
 #include <time.h>
+#include <iomanip>
+#include <iostream>
 
 double f(double x)
 {
-	return pow(x, 4.0) - 4 * pow(x, 3.0) + 9 * pow(x, 2.0) - 16 * x;
+	//return pow(x, 4.0) - 4 * pow(x, 3.0) + 9 * pow(x, 2.0) - 16 * x;
+	return 2*pow(x, 3.0) - 7 * x + 4;
 }
-
+using namespace std;
 int main(int &argc, char* argv[])
 {
-	clock_t start = clock();
+	srand(time(NULL));
+	#define N 1'000'000
 	double a = 4.0, b = 5.0;
-	
-	#define N 1'000'000'000
 	double step = (b - a) / N;
 	double sum = 0;
-	//// Option 1: time ~0.019 sec
-	//#pragma omp parallel 
-	//{
-	//	#pragma omp for reduction(+:sum)
-	//	for (int i = 0; i < N; ++i)
-	//	{
-	//		sum += step * f(a + step * i);
-	//	}
-	//}
-
-	// Option 2: time ~0.016 sec
+	auto begin = chrono::high_resolution_clock::now();
+	for (int i = 0; i < N; ++i)
+	{
+		sum += step * f(a + step * i);
+	}
+	auto end = chrono::high_resolution_clock::now();
+	cout << "(posl)res: " << sum << " :: " <<setw(10)<< chrono::duration_cast<chrono::nanoseconds>(end - begin).count() << "nsec\n";
+	sum = 0;
+	begin = chrono::high_resolution_clock::now();
 	#pragma omp parallel for reduction(+:sum)
 	for (int i = 0; i < N; ++i)
 	{
 		sum += step * f(a + step * i);
 	}
-	clock_t end = clock() - start;
-	printf("%f, time: %f", sum,((double) end / CLOCKS_PER_SEC));
-}
+
+	end = chrono::high_resolution_clock::now();
+	cout << "(par)res: " << sum << " :: " << setw(10) << chrono::duration_cast<chrono::nanoseconds>(end - begin).count() << "nsec\n";
+}	
+//clock_t start = clock();
+//clock_t end = clock() - start;
+	//printf("%f, time: %f", sum,((double) end / CLOCKS_PER_SEC));
+//// Option 1: time ~0.019 sec (N == 1'000'000, 12 threads processor)
+//#pragma omp parallel 
+//{
+//	#pragma omp for reduction(+:sum)
+//	for (int i = 0; i < N; ++i)
+//	{
+//		sum += step * f(a + step * i);
+//	}
+//}
+// Option 2: time ~0.016 sec (N == 1'000'000, 12 threads processor)
